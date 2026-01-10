982428dc444c96ebac090e7b3540e93118c8a163
Nevin Gregory <nevin.gregory2022@gmail.com>
2026-01-10 01:55:09 -0500

    Plant collisions, hunger, energy, ui

--- bobbles/src/main.rs
+++ bobbles/src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{post_process::bloom::Bloom, prelude::*};
+use bevy::{
+    post_process::bloom::Bloom, prelude::*
+};
 use rand::Rng;
 use rand::prelude::*;
 use std::collections::{HashMap, HashSet};
@@ -213,9 +215,20 @@ impl Genome {
 const TARGET_SPEED: f32 = 200.;
 /// How quickly should the camera snap to the desired location.
 const CAMERA_DECAY_RATE: f32 = 5.;
-const PLAYER_SCALE: f32 = 5.;
-const PLANT_SCALE: f32 = 5.;
-const HUNGER_TICK: f32 = 1.; // How much hunger decreases every gamestep
+const PLAYER_SCALE: f32 = 64.;
+const PLANT_SCALE: f32 = 32.;
+const HUNGER_RATE: f32 = 1.0; // How much hunger decreases every gamestep
+const HEALING_RATE: f32 = 0.2; 
+const ENERGY_RATE: f32 = 0.2; 
+const INITIAL_SPAWN: i32 = 20;
+const COLLISION_DISTANCE: f32 = 8.;
+const START_HEALING_TIME: f32 = 10.;
+const START_RESTING_TIME: f32 = 2.;
+
+const PREGNANCY_TIME: f32 = 20.;
+
+const MALE_COLOR: Color = Color::srgb(0., 0., 1.);
+const FEMALE_COLOR: Color = Color::srgb(1., 0., 1.);
 
 enum BobbleGender {
     Male,
@@ -242,29 +255,51 @@ struct Health {
     health: f32,
     max_health: f32,
     alive: bool,
+    timer: Timer,
 }
 
 #[derive(Component)]
 struct Energy {
     energy: f32,
     max_energy: f32,
+    timer: Timer,
 }
 
 #[derive(Component)]
-struct Plant {
+struct Plant;
+
+#[derive(Component)]
+struct Collider;
+
+#[derive(Component)]
+struct Edible {
     nutrition_value: f32,
-    poison_level: f32,
 }
 
 #[derive(Component)]
-struct Edible;
+struct Movement {
+    velocity: Vec2,
+    last_x: f32,
+    last_y: f32,
+}
+
+#[derive(Component)]
+struct Reproducing {
+    
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(MeshPickingPlugin) 
-        .add_systems(Startup, (setup_scene, setup_camera))
-        .add_systems(Update, ((update_health, update_hunger, despawn_dead).chain(), (move_target, update_camera).chain()))
+        .add_systems(Startup, (setup_scene, setup_camera, setup_ui))
+        .add_systems(Update, (
+            ((update_health, update_hunger, update_energy), despawn_dead).chain(),
+            (move_target, update_camera).chain(),
+            bobble_eating_collision,
+            update_ui,
+            update_velocity,
+        ))
         .run();
 }
 
@@ -272,35 +307,81 @@ fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     // World where we move the target
     commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(1000., 700.))),
-        MeshMaterial2d(materials.add(Color::srgb(0.2, 0.2, 0.3))),
+        Mesh2d(meshes.add(Rectangle::new(1000., 1000.))),
+        MeshMaterial2d(materials.add(Color::srgb(0.2, 0.2, -1.))),
     ));
 
     // Target
     commands.spawn((
         Target,
-        Transform::from_xyz(0., 0., 2.),
+        Transform::from_xyz(0., 0., 0.),
+        Bobble {
+            age: 10,
+            gender: BobbleGender::Male,
+        },
+        Sprite {
+            image: asset_server.load("human.png"),
+            color: Color::srgb(1., 0., 1.),
+            custom_size: Some(Vec2::new(PLAYER_SCALE, PLAYER_SCALE)),
+            ..default()
+        },
+        Pickable {
+            should_block_lower: true,
+            is_hoverable: true, 
+        },
+        Hunger {
+            hunger: 100.,
+            max_hunger: 100.,
+        },
+        Health {
+            health: 100.,
+            max_health: 100.,
+            alive: true,
+            timer: Timer::from_seconds(START_HEALING_TIME, TimerMode::Once),
+        },
+        Energy {
+            energy: 100.,
+            max_energy: 100.,
+            timer: Timer::from_seconds(START_RESTING_TIME, TimerMode::Once),
+        },
+        Movement {
+            velocity: Vec2::new(0.0, 0.0),
+            last_x: 0.0,
+            last_y: 0.0,
+        },
+        Collider,
     ));
 
     let mut rng = rand::rng();
-    for _ in 1..=100 {
-        let x: f32 = rng.random_range(-100_f32..=100_f32);
-        let y: f32 = rng.random_range(-100_f32..=100_f32);
+    for _ in 1..=INITIAL_SPAWN {
+        let x: f32 = rng.random_range(-500_f32..=500_f32);
+        let y: f32 = rng.random_range(-500_f32..=500_f32);
 
         let max_hunger: f32 = rng.random_range(50.0..=200.0);
         let max_health: f32 = rng.random_range(50.0..=200.0);
         let max_energy: f32 = rng.random_range(100.0..=150.0);
-        let start_color: Color = Color::srgb(0., 0., 1.);
         let hover_color: Color = Color::srgb(6.25, 9.4, 9.1);
+
+        let is_male: bool = rng.random_bool(0.5);
+        let start_color: Color = if is_male {
+            MALE_COLOR
+        } else {
+            FEMALE_COLOR
+        };
 
         //Bobble
         commands.spawn((
             Bobble {
                 age: 10,
-                gender: BobbleGender::Male,
+                gender: if is_male { 
+                    BobbleGender::Male
+                } else {
+                    BobbleGender::Female
+                },
             },
             Hunger {
                 hunger: max_hunger,
@@ -310,16 +391,21 @@ fn setup_scene(
                 health: max_health,
                 max_health: max_health,
                 alive: true,
+                timer: Timer::from_seconds(START_HEALING_TIME, TimerMode::Once),
             },
-            Mesh2d(meshes.add(Circle::new(PLAYER_SCALE))),
-            MeshMaterial2d(materials.add(start_color)),
-            Transform::from_xyz(x * PLAYER_SCALE, y * PLAYER_SCALE, 2.),
+            Sprite {
+                image: asset_server.load("human.png"),
+                color: start_color,
+                custom_size: Some(Vec2::new(PLAYER_SCALE, PLAYER_SCALE)),
+                ..default()
+            },
+            Transform::from_xyz(x, y, 0.),
             Pickable {
                 should_block_lower: true,
                 is_hoverable: true, 
             },
         ))
-        .observe(|trigger: Trigger<Pointer<Click>>, query: Query<(&Hunger, &Health)>| {
+        .observe(|trigger: On<Pointer<Click>>, query: Query<(&Hunger, &Health)>| {
             println!("Click");
             let clicked_entity = trigger.entity;
 
@@ -327,41 +413,72 @@ fn setup_scene(
                 println!("Hunger: {}, Health: {}", hunger.hunger, health.health);
             }
         })
-        .observe(move |trigger: Trigger<Pointer<Over>>, mut materials: ResMut<Assets<ColorMaterial>>, query: Query<&MeshMaterial2d<ColorMaterial>>| {
-            if let Ok(mat_handle) = query.get(trigger.entity) {
-                if let Some(mat) = materials.get_mut(&mat_handle.0) {
-                    mat.color = hover_color;
-                }
+        .observe(move |trigger: On<Pointer<Over>>, mut query: Query<&mut Sprite>| {
+            if let Ok(mut sprite_handle) = query.get_mut(trigger.entity) {
+                sprite_handle.color = hover_color;
             }
         })
-        .observe(move |trigger: Trigger<Pointer<Out>>, mut materials: ResMut<Assets<ColorMaterial>>, query: Query<&MeshMaterial2d<ColorMaterial>>| {
-            if let Ok(mat_handle) = query.get(trigger.entity) {
-                if let Some(mat) = materials.get_mut(mat_handle) {
-                    mat.color = start_color;
-                }
+        .observe(move |trigger: On<Pointer<Out>>, mut query: Query<&mut Sprite>| {
+            if let Ok(mut sprite_handle) = query.get_mut(trigger.entity) {
+                sprite_handle.color = start_color;
             }
         });
     }
 
     //Plant
+    for _ in 1..=INITIAL_SPAWN {
+        let x: f32 = rng.random_range(-500_f32..=500_f32);
+        let y: f32 = rng.random_range(-500_f32..=500_f32);
+
+        commands.spawn((
+            Plant,
+            Health {
+                health: 100.,
+                max_health: 100.,
+                alive: true,
+                timer: Timer::from_seconds(START_HEALING_TIME, TimerMode::Once),
+            },
+            Sprite {
+                image: asset_server.load("plant.png"),
+                color: Color::srgb(0., 1., 0.),
+                custom_size: Some(Vec2::new(PLANT_SCALE, PLANT_SCALE)),
+                ..default()
+            },
+            Edible {
+                nutrition_value: 100.,
+            },
+            Collider,
+            Transform::from_xyz(x, y, 0.),
+        ));
+    }
+}
+
+fn setup_ui(mut commands: Commands) {
     commands.spawn((
-        Plant {
-            nutrition_value: 100.,
-            poison_level: 100.,
+        Text::new("Hunger: 100\nHealth: 100\nEnergy: 100"),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: px(12),
+            left: px(12),
+            ..default()
         },
-        Health {
-            health: 100.,
-            max_health: 100.,
-            alive: true,
-        },
-        Hunger {
-            hunger: 100.,
-            max_hunger: 100.,
-        },
-        Mesh2d(meshes.add(Circle::new(PLANT_SCALE))),
-        MeshMaterial2d(materials.add(Color::srgb(0., 1., 0.))),
-        Transform::from_xyz(0., 3. * PLANT_SCALE, 2.),
     ));
+}
+
+fn update_ui(
+    mut text_query: Query<&mut Text>,
+    target_query: Single<(&Hunger, &Health, &Energy, &Movement), With<Target>>,
+) {
+    for mut text in text_query.iter_mut() {
+        *text = Text::new(format!(
+            "Hunger {:.2} Health {:.2} Energy {:.2} Velocity ({:.2}, {:.2})", 
+            target_query.0.hunger, 
+            target_query.1.health, 
+            target_query.2.energy, 
+            target_query.3.velocity.x, 
+            target_query.3.velocity.y
+        ));
+    }
 }
 
 fn setup_camera(mut commands: Commands) {
@@ -413,10 +530,11 @@ fn move_target(
 }
 
 fn update_hunger(
+    time: Res<Time>,
     mut hunger_query: Query<&mut Hunger>
 ) {
     hunger_query.iter_mut().for_each(|mut query| {
-        query.hunger -= HUNGER_TICK;
+        query.hunger -= HUNGER_RATE * time.delta_secs();
 
         if query.hunger <= 0. {
             query.hunger = 0.;
@@ -424,18 +542,60 @@ fn update_hunger(
     });
 }
 
+fn update_energy(
+    time: Res<Time>,
+    mut targets: Query<(&mut Energy, &Movement), With<Bobble>>
+) {
+    targets.iter_mut().for_each(|(mut energy, movement)| {
+        energy.timer.tick(time.delta());
+
+        // If moving, lose energy? Proportional to speed?
+        if movement.velocity.abs().length() > f32::EPSILON {
+            energy.energy -= ENERGY_RATE * time.delta_secs();
+            energy.timer.reset();
+        }
+
+        if energy.timer.is_finished() && energy.energy < energy.max_energy {
+            energy.energy += ENERGY_RATE * time.delta_secs();
+        }
+    });
+}
+
 fn update_health(
+    time: Res<Time>,
     mut targets: Query<(&mut Health, &Hunger), With<Hunger>>
 ) {
     targets.iter_mut().for_each(|(mut health, hunger)| {
+        health.timer.tick(time.delta());
+
         if hunger.hunger <= 0. {
-            health.health -= HUNGER_TICK;
+            health.health -= HUNGER_RATE * time.delta_secs();
+            health.timer.reset();
         }
 
         if health.health <= 0. {
             health.health = 0.;
             health.alive = false;
         }
+
+        if health.timer.is_finished() && health.health < health.max_health {
+            health.health += HEALING_RATE * time.delta_secs();
+        }
+    });
+}
+
+fn update_velocity(
+    time: Res<Time>,
+    mut targets: Query<(&Transform, &mut Movement)>
+) {
+    targets.iter_mut().for_each(|(transform, mut movement)| {
+        let delta_x = transform.translation.x - movement.last_x;
+        let delta_y = transform.translation.y - movement.last_y;
+        movement.velocity.x = delta_x / time.delta_secs();
+        movement.velocity.y = delta_y / time.delta_secs();
+
+        movement.last_x = transform.translation.x;
+        movement.last_y = transform.translation.y;
     });
 }
 
@@ -448,4 +608,49 @@ fn despawn_dead(
             commands.entity(entity).despawn();
         }
     });
+}
+
+fn bobble_eating_collision(
+    mut edible_collider_query: Query<(&mut Transform, &Edible), (With<Collider>, With<Edible>)>,
+    mut bobble_collider_query: Query<(&Transform, &mut Hunger), (With<Collider>, With<Bobble>, With<Target>, Without<Edible>)>,
+) {
+    edible_collider_query.iter_mut().for_each(|(mut edible_transform, edible)| {
+        bobble_collider_query.iter_mut().for_each(|(bobble_transform, mut hunger)| {
+            let dist = edible_transform.translation.truncate().distance(bobble_transform.translation.truncate());
+            if dist < COLLISION_DISTANCE {
+                // "Despawn" eaten thing (Move it somewhere else)
+                let mut rng = rand::rng();
+                let x: f32 = rng.random_range(-500_f32..=500_f32);
+                let y: f32 = rng.random_range(-500_f32..=500_f32);
+
+                edible_transform.translation = Vec3::new(x, y, 0.);
+                
+                hunger.hunger += edible.nutrition_value;
+                if hunger.hunger > hunger.max_hunger {
+                    hunger.hunger = hunger.max_hunger;
+                }
+            }
+        });
+    });
+}
+
+fn bobble_reproducing_collision(
+    mut bobble_query: Query<(&Transform, &Hunger, &Health, &Energy, &Bobble), (With<Reproducing>, With<Bobble>)>
+) {
+    let mut combinations = bobble_query.iter_combinations_mut();
+    while let Some([
+            (transform1, hunger1, health1, energy1, bobble1), 
+            (transform2, hunger2, health2, energy2, bobble2)
+        ]) = combinations.fetch_next() {
+        if bobble1.gender != bobble2.gender { 
+            // One is male, one is female
+            let dist = transform1.translation.truncate().distance(transform2.translation.truncate());
+            if dist < COLLISION_DISTANCE {
+                // They're close enough...
+                let mut rng = rand::rng();
+                let reproductibility_score = 
+            }
+            // Now we combine them
+        }
+    }
 }
